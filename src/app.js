9faf6d725197f397af2982b85a775f78e80d2410
santialturria <alturriasantiagoezequiel@gmail.com>
2025-12-12 23:52:07 -0300

    Estructura inicial backend con Express

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const productsRouter = require("./routes/products.router");
+const cartsRouter = require("./routes/carts.router");
+
+const app = express();
+const PORT = 8080;
+
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartsRouter);
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en puerto ${PORT}`);
+});
